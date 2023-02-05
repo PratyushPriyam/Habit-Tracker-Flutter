9e2d6960baa0458c3bb236a4dd5354f8b4d0975c
parthnautiyal <parthnautiyal2002@gmail.com>
2023-02-06 00:11:32 +0530

    added main_page,login_page,home_page integrated firebase to manually add and sign in users

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,109 +16,111 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.grey[300],
         body: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.android_rounded,
-                  size: 200,
-                ),
-                SizedBox(height: 75),
-                //Hello
-                Text(
-                  'Hello Again!',
-                  style: GoogleFonts.bebasNeue(
-                    fontSize: 54,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.android_rounded,
+                    size: 200,
                   ),
-                ),
-                SizedBox(height: 20),
-                Text("Welcome Back! You've been missed",
-                    style: TextStyle(fontSize: 20)),
-
-                SizedBox(height: 50),
-                //Email text field
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
+                  SizedBox(height: 75),
+                  //Hello
+                  Text(
+                    'Hello Again!',
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: 54,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
+                  ),
+                  SizedBox(height: 20),
+                  Text("Welcome Back! You've been missed",
+                      style: TextStyle(fontSize: 20)),
+
+                  SizedBox(height: 50),
+                  //Email text field
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Email',
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 10),
-                //password textfield
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Password',
+                  SizedBox(height: 10),
+                  //password textfield
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Password',
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 15),
+                  SizedBox(height: 15),
 
-                //sign in button
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(12),
+                  //sign in button
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text('Sign in',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18)),
+                      ),
                     ),
-                    child: Center(
-                      child: Text('Sign in',
+                  ),
+                  SizedBox(height: 25),
+
+                  //not a member? register now
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Not a member?',
                           style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18)),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 25),
-
-                //not a member? register now
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Not a member?',
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(
+                        ' Register Now',
                         style: TextStyle(
+                          color: Colors.blue,
                           fontWeight: FontWeight.bold,
-                        )),
-                    Text(
-                      ' Register Now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ],
-                )
-              ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ));
